eac689dab4216ccb85a6cfc78bae22c4fe37d3e1
vitaliy <vitality.boldorev@yandex.ru>
2023-03-19 13:15:53 +0300

    first xommit

---
+++ Algorithms/NaiveAlgorithm.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithms
+{
+    public static class NaiveAlgorithm
+    {
+        public static List<Tuple<int, int>> Execution(string mainStr, string subStr)
+        {
+            var count = 0;
+            var res = new List<Tuple<int, int>>();
+
+            if (mainStr.Length < subStr.Length)
+            {
+                throw new ArgumentException("Длина основной строки должна быть больше длины подстроки!");
+            }
+
+            for (var i = 0; i + subStr.Length - 1 < mainStr.Length; i++)
+            {
+                bool isCompleteCoincidence = true;
+                for (var j = 0; j < subStr.Length; j++)
+                {
+                    isCompleteCoincidence = mainStr[i + j] == subStr[j];
+                    count++;
+                    if (!isCompleteCoincidence)
+                    {
+                        break;
+                    }
+                }
+                if (isCompleteCoincidence)
+                {
+                    res.Add(Tuple.Create(i, count));
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleTables;
+
+do
+{
+    Console.Write("Основная строка: ");
+    var mainStr = Console.ReadLine();
+    Console.Write("Подстрока: ");
+    var subStr = Console.ReadLine();
+
+    try
+    {
+        var res = Algorithms.NaiveAlgorithm.Execution(mainStr, subStr);
+
+        var firstLine = new List<string>();
+        var secondLine = new List<string>();
+        for (int i = 0; i < mainStr.Length; i++)
+        {
+            firstLine.Add(i.ToString());
+            secondLine.Add(mainStr[i].ToString());
+        }
+        var table = new ConsoleTable(firstLine.ToArray());
+        table.AddRow(secondLine.ToArray());
+
+        table.Write();
+        Console.WriteLine();
+
+        foreach (var item in res)
+        {
+            Console.WriteLine($"Индекс вхождения: {item.Item1}, Кол-во сравнений: {item.Item2}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+
+    Console.WriteLine("Завершить работу? (Esc)");
+} while (Console.ReadKey().KeyChar != (char)ConsoleKey.Escape);
